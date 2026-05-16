c0ab059bce72deefc2795080cfa47e505dec6716
kevin <kevin.douglas.disher@gmail.com>
2026-05-16 15:06:52 -0700

    fix(#7): update PodcastsPage for TrackListResponse type

--- frontend/src/pages/PodcastsPage.tsx
+++ frontend/src/pages/PodcastsPage.tsx
@@ -5,12 +5,12 @@ import TrackList from "../components/TrackList";
 export default function PodcastsPage() {
   const [tracks, setTracks] = useState<Track[]>([]);
   useEffect(() => {
-    api.tracks("podcast", 500).then(setTracks).catch(() => {});
+    api.tracks("podcast", 500).then((res) => setTracks(res.tracks)).catch(() => {});
   }, []);
   return (
     <div className="space-y-4">
       <h1 className="text-2xl font-semibold">Podcasts</h1>
-      <TrackList tracks={tracks} onDelete={() => api.tracks("podcast", 500).then(setTracks)} />
+      <TrackList tracks={tracks} onDelete={() => api.tracks("podcast", 500).then((res) => setTracks(res.tracks))} />
     </div>
   );
 }
